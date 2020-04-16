2a9790d847db0de5c9ea350b08dd401a6215708c
weimeilin79 <weimielin@gmail.com>
2020-04-16 17:28:08 -0400

    update

--- kamel101/env-init.sh
+++ kamel101/env-init.sh
@@ -1,3 +1,4 @@
-ssh root@host01 "wget https://github.com/apache/camel-k/releases/download/1.0.0-RC2/camel-k-client-1.0.0-RC2-linux-64bit.tar.gz"
-ssh root@host01 "tar -xvf camel-k-client-1.0.0-RC2-linux-64bit.tar.gz"
-ssh root@host01 "mv kamel /usr/bin"
+#!/bin/bash
+wget https://github.com/apache/camel-k/releases/download/1.0.0-RC2/camel-k-client-1.0.0-RC2-linux-64bit.tar.gz
+tar -xvf camel-k-client-1.0.0-RC2-linux-64bit.tar.gz
+mv kamel /usr/bin
