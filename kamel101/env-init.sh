eafe1c4f99aacefa394576dd373e9e1573d60568
weimeilin79 <weimielin@gmail.com>
2020-04-16 17:07:05 -0400

    Add Kamel Binary to env-init.sh

---
+++ kamel101/env-init.sh
@@ -0,0 +1,3 @@
+ssh root@host01 "wget https://github.com/apache/camel-k/releases/download/1.0.0-RC2/camel-k-client-1.0.0-RC2-linux-64bit.tar.gz"
+ssh root@host01 "tar -xvf camel-k-client-1.0.0-RC2-linux-64bit.tar.gz"
+ssh root@host01 "mv kamel /usr/bin"
